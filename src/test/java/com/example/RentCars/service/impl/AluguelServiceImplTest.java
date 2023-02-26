e3ed6348f13bed8c2ea78783463f014949f89f85
Allan.Souza <km.allansouza@gmail.com>
2023-02-26 01:37:02 -0300

    Implementação base para testes.

---
+++ src/test/java/com/example/RentCars/service/impl/AluguelServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.example.RentCars.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AluguelServiceImplTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
